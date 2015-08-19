c611c0a457608a6d79d8c1c1690b4ff081b1997b
Tyrantz <raymanv3@gmail.com>
2015-08-19 04:26:41 +0000

    adds modal window to html to output score, and adds basic score functionality to main.js

---
+++ includes/main.js
@@ -0,0 +1,21 @@
+var answer_values = [];
+var correct = 0;
+
+$(document).ready (function () {
+  $('button.get-score').click (function () {
+    // pushes all incorrect & correct values to array
+    $(':radio:checked').each (function () {
+      // console.log($(this).val());
+      answer_values.push($(this).val());
+    });
+    // count the correct values
+    answer_values.forEach (function (value) {
+      console.log(value);
+    });
+    answer_values.forEach (function (data) {
+      if (data == "correct") correct++;
+    });
+    console.log(correct);
+    $('.display-score').text(correct);
+  });
+});
