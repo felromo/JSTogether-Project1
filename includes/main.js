24f1fa405e6d10a3204a4870d881fbdee763ab53
Tyrantz <raymanv3@gmail.com>
2015-08-19 09:03:28 +0000

    fixes correct number bug

--- includes/main.js
+++ includes/main.js
@@ -1,9 +1,11 @@
-var answer_values = [];
+var answer_values;
 var total_questions = 5;
-var correct = 0;
+var correct;
 
 $(document).ready (function () {
   $('button.get-score').click (function () {
+    answer_values = [];
+    correct = 0;
     // pushes all incorrect & correct values to array
     $(':radio:checked').each (function () {
       // console.log($(this).val());
@@ -11,12 +13,10 @@ $(document).ready (function () {
     });
     // count the correct values
     answer_values.forEach (function (value) {
-      console.log(value);
     });
     answer_values.forEach (function (data) {
       if (data == "correct") correct++;
     });
-    console.log(correct);
     $('.display-score').text(correct + "/" + total_questions + " correct");
   });
 });
